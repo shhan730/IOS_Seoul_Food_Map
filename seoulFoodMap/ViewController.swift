6c2b66a3612b73fa36ad7814633cdd746acde01a
Seung Hun Han <shhan730@gmail.com>
2019-08-24 20:52:19 +0900

    Add Camera Locating Methods

--- seoulFoodMap/ViewController.swift
+++ seoulFoodMap/ViewController.swift
@@ -15,21 +15,40 @@ import CoreLocation
 class ViewController: UIViewController {
 
     fileprivate let locationManager: CLLocationManager = CLLocationManager()
+    private var currentCordinate: CLLocationCoordinate2D?
 
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.distanceFilter = kCLDistanceFilterNone
-        locationManager.startUpdatingLocation()
-        
-        mapView.showsUserLocation = true
         viewSetting()
-//        locationManager.enableLocationServices()
+        configureLocationServices()
         
         
+    }
+    
+    func configureLocationServices() {
+        locationManager.delegate = self
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .notDetermined{
+            locationManager.requestWhenInUseAuthorization()
+        }else if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginLocationUpdate(locationManager: locationManager)
+        }
+    }
+    
+    private func zoomToLatestLocation(with cordinate: CLLocationCoordinate2D) {
+        let zoomRegion = MKCoordinateRegion.init(center: cordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion(zoomRegion, animated: true)
+    }
+    
+    private func beginLocationUpdate(locationManager: CLLocationManager) {
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
     }
     
     func viewSetting() {
@@ -39,5 +58,28 @@ class ViewController: UIViewController {
         
     }
     
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("did get latest location")
+        
+        guard let latestLocation = locations.first else { return }
+        
+        if currentCordinate == nil {
+            zoomToLatestLocation(with: latestLocation.coordinate)
+        }
+        
+        currentCordinate = latestLocation.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("status changed")
+        if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginLocationUpdate(locationManager: manager)
+        }
+    }
     
 }
